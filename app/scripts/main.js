2918471237c2154eaeed0b2ba3decc008acababa
Joanna Edgell <joedgell2013@gmail.com>
2014-10-09 08:07:44 -0400

    spacing worked out for main body

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -12,7 +12,7 @@ var final_rendered_user_html = renderedUser(userInfo);
 //Right side main info
 
 
-var temp = $('#repo_left_template').html();
+var temp = $('#repo_template').html();
 
 var rendered =_.template(temp);
 
@@ -24,22 +24,6 @@ _.each(repository, function(item){
 
   final_rendered_html = rendered(item);
 
-  $('.leftBox').append(final_rendered_html);
-
-});
-
-var temp = $('#repo_right_template').html();
-
-var rendered =_.template(temp);
-
-var final_rendered_html;
-
-repository.reverse();
-
-_.each(repository, function(item){
-
-  final_rendered_html = rendered(item);
-
-  $('.leftBox').append(final_rendered_html);
+  $('.box').append(final_rendered_html);
 
 });
