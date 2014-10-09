18a5ca85980db958b393b97a6bb00269d606d543
Joanna Edgell <joedgell2013@gmail.com>
2014-10-09 15:35:20 -0400

    one live showing up

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,4 @@
+/*s
 
 //Sidebar Main info
 
@@ -24,6 +25,49 @@ _.each(repository, function(item){
 
   final_rendered_html = rendered(item);
 
-  $('.box').append(final_rendered_html);
+  $('.repo_info').append(final_rendered_html);
 
+});*/
+
+
+//Sidebar Main info
+
+
+
+var user_url = 'https://api.github.com/users/joedge';
+
+var star_url = 'https://api.github.com/users/joedge/starred';
+
+var org_url = 'https://api.github.com/users/joedge/orgs';
+
+var tempUser = $('#user_template').html();
+
+var renderedUser =_.template(tempUser);
+
+// var final_rendered_user_html = renderedUser(user_url);
+
+  $.getJSON(user_url).done( function(i){
+
+    $('.sidebar').append(renderedUser(i));
+
+});
+
+
+
+
+
+
+
+
+//Right side main info
+var repo_url = 'https://api.github.com/users/joedge/repos';
+
+var temp = $('#repo_template').html();
+
+var rendered =_.template(temp);
+
+
+  $.getJSON(repo_url).done(function(i){
+
+  $('.repo_info').append(rendered(i));
 });
