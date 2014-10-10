ac622b0cff281fcf7717de2c18a80d37405098f7
Joanna Edgell <joedgell2013@gmail.com>
2014-10-10 11:03:05 -0400

    everything live

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,12 @@
-//Sidebar Main info
+//API info
 
 var star_url = 'https://api.github.com/users/joedge/starred';
 
 var org_url = 'https://api.github.com/users/joedge/orgs';
 
 var user_url = 'https://api.github.com/users/joedge';
+
+var repo_url = 'https://api.github.com/users/joedge/repos';
 
 //Header info
 var tempHeader = $('#header_template').html();
@@ -18,19 +20,67 @@ var renderedHeader =_.template(tempHeader);
 });
 
 
-//Side bar info
-var tempUser = $('#user_template').html();
+//Side bar top info
+var tempUser = $('#user_top_template').html();
 
 var renderedUser =_.template(tempUser);
 
   $.getJSON(user_url).done( function(user_data){
 
-    $('.sidebar').append(renderedUser(user_data));
+    $('.sidebar_top').append(renderedUser(user_data));
+
+});
+//Sidebar middle
+
+var tempMiddle1 = $('#user_middle_template1').html();
+
+var renderedMiddle1 =_.template(tempMiddle1);
+
+  $.getJSON(user_url).done( function(user_data){
+
+    $('.sidebar_middle').append(renderedMiddle1(user_data));
+
+});
+
+var tempMiddle2 = $('#user_middle_template2').html();
+
+var renderedMiddle2 =_.template(tempMiddle2);
+
+  $.getJSON(star_url).done( function(star_data){
+
+    _.each(star_data, function(a){
+
+    $('.sidebar_middle').append(renderedMiddle2(a));
+})
+
+});
+
+
+var tempMiddle3 = $('#user_middle_template3').html();
+
+var renderedMiddle3 =_.template(tempMiddle3);
+
+  $.getJSON(user_url).done( function(user_data){
+
+    $('.sidebar_middle').append(renderedMiddle3(user_data));
+
+});
+
+//Sidebar bottom info
+var tempOrg = $('#user_bottom_template').html();
+
+var renderedOrg =_.template(tempOrg);
+
+  $.getJSON(org_url).done( function(org_data){
+
+    _.each(org_data, function(a){
+
+    $('.sidebar_bottom').append(renderedOrg(a));
+})
 
 });
 
 //Right side main info
-var repo_url = 'https://api.github.com/users/joedge/repos';
 
 var temp = $('#repo_template').html();
 
