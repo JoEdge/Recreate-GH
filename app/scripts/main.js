eccc88bcef0e47bc58160349728f75ff3471461e
Joanna Edgell <joedgell2013@gmail.com>
2014-10-09 21:49:43 -0400

    Getting there

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,59 +1,33 @@
-/*s
-
 //Sidebar Main info
-
-var tempUser = $('#user_template').html();
-
-var renderedUser =_.template(tempUser);
-
-var final_rendered_user_html = renderedUser(userInfo);
-
-  $('.sidebar').append(final_rendered_user_html);
-
-//Right side main info
-
-
-var temp = $('#repo_template').html();
-
-var rendered =_.template(temp);
-
-var final_rendered_html;
-
-repository.reverse();
-
-_.each(repository, function(item){
-
-  final_rendered_html = rendered(item);
-
-  $('.repo_info').append(final_rendered_html);
-
-});*/
-
-
-//Sidebar Main info
-
-
-
-var user_url = 'https://api.github.com/users/joedge';
 
 var star_url = 'https://api.github.com/users/joedge/starred';
 
 var org_url = 'https://api.github.com/users/joedge/orgs';
 
+var user_url = 'https://api.github.com/users/joedge';
+
+//Header info
+var tempHeader = $('#header_template').html();
+
+var renderedHeader =_.template(tempHeader);
+
+  $.getJSON(user_url).done( function(header_data){
+
+    $('.header_image').append(renderedHeader(header_data));
+
+});
+
+
+//Side bar info
 var tempUser = $('#user_template').html();
 
 var renderedUser =_.template(tempUser);
-
-// var final_rendered_user_html = renderedUser(user_url);
 
   $.getJSON(user_url).done( function(user_data){
 
     $('.sidebar').append(renderedUser(user_data));
 
-
 });
-
-
 
 //Right side main info
 var repo_url = 'https://api.github.com/users/joedge/repos';
